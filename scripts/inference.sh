fcb6c1821ac161a61929b451d67dd6aedf31ec61
Rohan Sawahn <49531442+rohansaw@users.noreply.github.com>
2023-08-22 09:56:49 +0200

    Update inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -5,9 +5,14 @@ CKPT=$2
 VIDEO=$3
 OUTDIR=${4:-"./examples/res"}
 
-python scripts/demo_inference.py \
+if [[ $(lshw -C display | grep vendor) =~ Nvidia ]]; then
+  do python scripts/demo_inference.py \
     --cfg ${CONFIG} \
     --checkpoint ${CKPT} \
     --video ${VIDEO} \
     --outdir ${OUTDIR} \
     --detector yolo  --save_img --save_video
+else
+  do echo "No GPU available"
+fi
+
